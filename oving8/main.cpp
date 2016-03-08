c4310c48c19216e29d2400160322d2d15c6f5c54
Kristian Rekstad <krirek@msn.com>
2016-03-08 08:26:39 +0100

    complete o8. add output file

--- oving8/main.cpp
+++ oving8/main.cpp
@@ -11,6 +11,9 @@ Vår 2016
 
 #include "CourseCatalog.h"
 
+#include <sstream>
+#include <map>
+
 using namespace std;
 
 // 1a
@@ -21,11 +24,14 @@ void copyFileWithLineNumbers(const char* inPath, const char* outPath);
 // 2a
 void printCharStatistics(const char* path);
 
+// 4a
+void wordStatistics(const char* path);
+
 int main() 
 {
-	//saveWordsToFile("output.txt");
-	//copyFileWithLineNumbers("input.txt", "inputLineNumbers.txt");
-	//printCharStatistics("bokstavStatistikkInput.txt");
+	saveWordsToFile("output.txt");
+	copyFileWithLineNumbers("input.txt", "inputLineNumbers.txt");
+	printCharStatistics("bokstavStatistikkInput.txt");
 
 	// 3c
 	CourseCatalog catalog;
@@ -60,6 +66,10 @@ int main()
 	catalogInput.close();
 	catalogFromFile.printCourses();
 
+
+	// 4a
+	cout << endl << endl;
+	wordStatistics("bokstavStatistikkInput.txt");
 
 	cin.ignore();
 	cin.get();
@@ -161,4 +171,93 @@ void printCharStatistics(const char * path)
 	cout << endl;
 
 	input.close();
+}
+
+// Fjerner alle tegn utenfor a-z, og gjør ordet til små bokstaver. 
+// Returnerer tom streng ved ord som f.eks. "å" eller "-".
+inline string& cleanUpWord(string &word)
+{
+	char *letters = new char[word.length() + 1];  // +1 for \0
+	size_t index = 0;
+
+	for (char &letter : word)
+	{
+		letter = tolower(letter);
+		if (letter >= 'a' && letter <= 'z')
+		{
+			letters[index++] = letter;
+		}
+	}
+	letters[index] = '\0';
+	
+	word = string(letters);
+
+	delete[] letters;
+	return word;
+}
+
+void wordStatistics(const char * path)
+{
+	ifstream input{ path };
+	
+	if (input.fail())
+	{
+		cout << "Failed to open " << path << endl;
+		return;
+	}
+
+	/*
+	Antall ord i filen
+	Antall ganger hvert ord forekommer
+	Hvilket ord som er lengst
+	Antall linjer i filen
+	*/
+
+	map<string, int> wordCounts;  // teller forekomst av hvert ord
+	long wordCount = 0;  // teller hvor mange ord totalt
+	string longestWord = "";
+	long lineCount = 0;
+	long totalWordLength = 0;  // for average length
+
+	string line;
+	string word;
+	while (getline(input, line))
+	{
+		lineCount++;
+
+		stringstream ss{ line };
+		while (ss >> word)
+		{
+			// Fjern rare tegn (æøå-, !"# etc.)
+			cleanUpWord(word);
+
+			// Skip "tomme" ord som f.eks.: å - (3.14)
+			if (word.length() == 0)
+			{
+				continue;
+			}
+
+			// Oppdater statistikk
+			totalWordLength += word.length();
+			wordCounts[word]++;
+			wordCount++;
+			if (word.length() > longestWord.length())
+			{
+				longestWord = word;
+			}
+		}
+	}
+
+	input.close();
+
+	cout << "Text statistics:" << endl
+		<< "Longest word: " << longestWord << endl
+		<< "Number of words: " << wordCount << endl
+		<< "Number of lines: " << lineCount << endl
+		<< "Average word length: " << totalWordLength / wordCount << endl;
+	
+	for (auto& word : wordCounts)
+	{
+		cout << word.first << ": " << word.second << endl;
+	}
 }
---
+++ oving8/readme.txt
@@ -0,0 +1,79 @@
+## Output fra main.cpp
+
+Enter words to save to output.txt
+Type quit to quit
+hehe
+test hei
+hva skjer
+yooo
+quit
+Invalid character ., skipping.
+Invalid character (, skipping.
+Invalid character û, skipping.
+Invalid character ), skipping.
+Invalid character Õ, skipping.
+Invalid character Õ, skipping.
+Invalid character Õ, skipping.
+Invalid character (, skipping.
+Invalid character ), skipping.
+Invalid character ., skipping.
+Character statistics:
+Total number of characters: 246 (236 valid)
+a: 21   b: 7    c: 1    d: 5    e: 36   f: 4    g: 11   h: 3    i: 5    j: 1
+k: 14   l: 20   m: 8    n: 14   o: 20   p: 3    q: 0    r: 20   s: 12   t: 15
+u: 3    v: 11   w: 1    x: 0    y: 0    z: 1
+All courses:
+TDT4102: Prosedyre- og objektorientert programmering
+TDT4110: Informasjonsteknologi grunnkurs
+TMA4100: Matematikk 1
+All courses:
+TDT4102: C++
+TDT4110: Informasjonsteknologi grunnkurs
+TMA4100: Matematikk 1
+
+Serializing catalog to file
+Deserializing catalog from file
+All courses:
+TDT4102: C++
+TDT4110: Informasjonsteknologi grunnkurs
+TMA4100: Matematikk 1
+
+
+Text statistics:
+Longest word: forskjellige
+Number of words: 42
+Number of lines: 4
+Average word length: 5
+alle: 1
+antall: 2
+az: 1
+begrense: 1
+bokstav: 1
+bokstavene: 1
+bokstaver: 4
+du: 3
+engelske: 1
+filen: 1
+forekommer: 1
+forenkle: 1
+forskjellige: 1
+ganger: 1
+hver: 1
+hvor: 1
+i: 1
+kan: 2
+konvertere: 1
+mange: 1
+med: 1
+normale: 1
+og: 2
+ogs: 1
+oppgaven: 1
+programmet: 1
+skal: 1
+sm: 1
+telle: 1
+teller: 1
+til: 2
+tolowerchar: 1
+ved: 1
